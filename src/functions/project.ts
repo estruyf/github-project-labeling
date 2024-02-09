899b3175b7c91f7b64b73a2fd8db413a6b617f0c
Elio Struyf <elio@struyfconsulting.be>
2024-02-09 13:28:40 +0100

    Added try/catch

--- src/functions/project.ts
+++ src/functions/project.ts
@@ -42,118 +42,126 @@ export async function project(
   );
 
   if (change.action === "created" || change.action === "edited") {
-    // Get project information
-    const cardData = await projectOctokit.graphql<CardData>(
-      queryCard(change.projects_v2_item.node_id)
-    );
+    try {
+      // Get project information
+      const cardData = await projectOctokit.graphql<CardData>(
+        queryCard(change.projects_v2_item.node_id)
+      );
 
-    // Create the repository app connection
-    const repoApp = new App({
-      appId: process.env.GITHUB_APP_ID,
-      privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
-    });
+      // Create the repository app connection
+      const repoApp = new App({
+        appId: process.env.GITHUB_APP_ID,
+        privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
+      });
 
-    const owner = cardData.node.content.repository.owner.login;
-    const repo = cardData.node.content.repository.name;
-    const issueId = cardData.node.content.number;
-    const issueLabels = cardData.node.content.labels.nodes;
-    const projectLabels = cardData.node.project.field.options;
-    const crntLabel = cardData.node.fieldValueByName.name;
+      const owner = cardData.node.content.repository.owner.login;
+      const repo = cardData.node.content.repository.name;
+      const issueId = cardData.node.content.number;
+      const issueLabels = cardData.node.content.labels.nodes;
+      const projectLabels = cardData.node.project.field.options;
+      const crntLabel = cardData.node.fieldValueByName.name;
 
-    // Get the installation ID from my personal account
-    const { data: installation } =
-      await repoApp.octokit.rest.apps.getRepoInstallation({
+      // Get the installation ID from my personal account
+      const { data: installation } =
+        await repoApp.octokit.rest.apps.getRepoInstallation({
+          owner,
+          repo,
+        });
+
+      const repoOctokit = await repoApp.getInstallationOctokit(installation.id);
+
+      // Get the labels
+      const { data: labels } = await repoOctokit.rest.issues.listLabelsForRepo({
         owner,
         repo,
       });
 
-    const repoOctokit = await repoApp.getInstallationOctokit(installation.id);
+      const repoLabels = labels.map((label) => label.name);
 
-    // Get the labels
-    const { data: labels } = await repoOctokit.rest.issues.listLabelsForRepo({
-      owner,
-      repo,
-    });
+      // Add the project label
+      await addProjectLabel(
+        repoOctokit,
+        owner,
+        repo,
+        issueId,
+        repoLabels,
+        cardData.node.project.title,
+        `Project: ${cardData.node.project.title}`
+      );
 
-    const repoLabels = labels.map((label) => label.name);
+      // Add the new project status label
+      await addLabel(
+        repoOctokit,
+        owner,
+        repo,
+        issueId,
+        repoLabels,
+        crntLabel,
+        projectLabels
+      );
 
-    // Add the project label
-    await addProjectLabel(
-      repoOctokit,
-      owner,
-      repo,
-      issueId,
-      repoLabels,
-      cardData.node.project.title,
-      `Project: ${cardData.node.project.title}`
-    );
-
-    // Add the new project status label
-    await addLabel(
-      repoOctokit,
-      owner,
-      repo,
-      issueId,
-      repoLabels,
-      crntLabel,
-      projectLabels
-    );
-
-    // Remove the labels from the issue that are not the current status
-    await removeLabels(
-      repoOctokit,
-      owner,
-      repo,
-      issueId,
-      issueLabels.map((label) => label.name),
-      projectLabels,
-      crntLabel
-    );
+      // Remove the labels from the issue that are not the current status
+      await removeLabels(
+        repoOctokit,
+        owner,
+        repo,
+        issueId,
+        issueLabels.map((label) => label.name),
+        projectLabels,
+        crntLabel
+      );
+    } catch (e) {
+      context.log("Error on create/edit", e.message);
+    }
   } else if (change.action === "deleted") {
-    const issueData = await projectOctokit.graphql<{ node: IssueContent }>(
-      queryIssue(change.projects_v2_item.content_node_id)
-    );
+    try {
+      const issueData = await projectOctokit.graphql<{ node: IssueContent }>(
+        queryIssue(change.projects_v2_item.content_node_id)
+      );
 
-    const projectData = await projectOctokit.graphql<{ node: Project }>(
-      queryProject(change.projects_v2_item.project_node_id)
-    );
+      const projectData = await projectOctokit.graphql<{ node: Project }>(
+        queryProject(change.projects_v2_item.project_node_id)
+      );
 
-    // Create the repository app connection
-    const repoApp = new App({
-      appId: process.env.GITHUB_APP_ID,
-      privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
-    });
-
-    const owner = issueData.node.repository.owner.login;
-    const repo = issueData.node.repository.name;
-    const issueId = issueData.node.number;
-
-    // Get the installation ID from my personal account
-    const { data: installation } =
-      await repoApp.octokit.rest.apps.getRepoInstallation({
-        owner,
-        repo,
+      // Create the repository app connection
+      const repoApp = new App({
+        appId: process.env.GITHUB_APP_ID,
+        privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
       });
 
-    const repoOctokit = await repoApp.getInstallationOctokit(installation.id);
+      const owner = issueData.node.repository.owner.login;
+      const repo = issueData.node.repository.name;
+      const issueId = issueData.node.number;
 
-    const issueLabels = issueData.node.labels.nodes;
-    const projectLabels = projectData.node.field.options;
-    await removeLabels(
-      repoOctokit,
-      owner,
-      repo,
-      issueId,
-      issueLabels.map((label) => label.name),
-      projectLabels
-    );
+      // Get the installation ID from my personal account
+      const { data: installation } =
+        await repoApp.octokit.rest.apps.getRepoInstallation({
+          owner,
+          repo,
+        });
 
-    await repoOctokit.rest.issues.removeLabel({
-      owner,
-      repo,
-      issue_number: issueId,
-      name: projectData.node.title,
-    });
+      const repoOctokit = await repoApp.getInstallationOctokit(installation.id);
+
+      const issueLabels = issueData.node.labels.nodes;
+      const projectLabels = projectData.node.field.options;
+      await removeLabels(
+        repoOctokit,
+        owner,
+        repo,
+        issueId,
+        issueLabels.map((label) => label.name),
+        projectLabels
+      );
+
+      await repoOctokit.rest.issues.removeLabel({
+        owner,
+        repo,
+        issue_number: issueId,
+        name: projectData.node.title,
+      });
+    } catch (e) {
+      context.log("Error on delete", e.message);
+    }
   }
 
   return { body: `OK` };
